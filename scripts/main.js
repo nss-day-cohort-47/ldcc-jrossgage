161abd8901ae0b18cdf2932a346ff7a64e8f21a7
Joel <jrossgage@gmail.com>
2021-03-26 13:54:46 -0500

    Added admin false for new registered users

--- scripts/main.js
+++ scripts/main.js
@@ -39,8 +39,10 @@ applicationElement.addEventListener("click", event => {
 		//collect all the details into an object
 		const userObject = {
 			name: document.querySelector("input[name='registerName']").value,
-			email: document.querySelector("input[name='registerEmail']").value
+			email: document.querySelector("input[name='registerEmail']").value,
+			isAdmin: false
 		}
+		console.log(userObject)
 		registerUser(userObject)
 			.then(dbUserObj => {
 				sessionStorage.setItem("user", JSON.stringify(dbUserObj));
